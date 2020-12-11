a7f390445aa7429e6dc27e8e5717659d361256cc
Catalin Sava <catalin.sava@ctce.ro>
2020-12-11 10:29:53 +0200

    commit

--- Startup.cs
+++ Startup.cs
@@ -26,8 +26,8 @@ namespace CTPapuci
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            services.AddDbContext<CTPapuciContext>(options =>
+           services.AddControllersWithViews();
+           services.AddDbContext<CTPapuciContext>(options =>
            options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
 
 
